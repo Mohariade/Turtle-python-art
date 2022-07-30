9941c71c8166091bb5c8e252350133a031d84f49
Mohariade <109514829+Mohariade@users.noreply.github.com>
2022-07-30 23:03:00 +0100

    Add files via upload
    Let's see together what you can draw only with circles 🌟

---
+++ circle's_art.py
@@ -0,0 +1,24 @@
+#Hello for evey one :) ... I wanna share with u this amazing drawing 'circle art' with turtle lib ...try it yourself in your python IDE and tell us what you think  if you enjoyed it Don't forget to share repo with your friends please ;)
+
+# insta : @_moha_wb
+
+
+import turtle
+
+#background
+wn=turtle.Screen()
+wn.bgcolor("#ffdd9a") 
+
+#editing pen of drawing
+t=turtle.Pen()
+t.speed(0)
+t.color("#0e1e52")
+t.width(3)
+
+#starting drawing
+for i in range(18):
+	t.left(20)
+	for j in range(50,60,2):
+		t.circle(j*2)
+
+turtle.mainloop()   
